2d42d3735f886cd15ab3ee11b9ea2bffc39be2a2
B18a <andreasjehle89@web.de>
2024-08-02 15:50:48 +0200

    end

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,2 +1,28 @@
 #include"Contact.hpp"
-#include"Phonebook.hpp"
+#include"PhoneBook.hpp"
+#include <iostream>
+
+#include <unistd.h> 
+#include <chrono> //time
+#include <thread>
+
+// using namespace std;
+
+
+int main (void)
+{
+	std::string input;
+
+	while(1)
+	{
+		std::cout<<"Enter a Command : [SEARCH] [ADD] [EXIT]" << std::endl;
+
+		std::getline(std::cin, input);	
+
+
+
+
+
+    	std::this_thread::sleep_for(std::chrono::microseconds(1000000)); // sleep for 1,000,000 microseconds (1 second)
+	}
+}
