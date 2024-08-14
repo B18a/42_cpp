d4f0629358109dc16e16aee8fb8a8518b6bfb682
Andreas Jehle <ajehle@2-D-1.42heilbronn.de>
2024-08-14 20:55:14 +0200

    start of harl

---
+++ c01/ex05/src/Harl.cpp
@@ -0,0 +1,35 @@
+#include"Harl.hpp"
+
+
+Harl::Harl(void){};
+Harl::~Harl(void){};
+
+void	Harl::complain(std::string level)
+{
+	
+}
+
+void	Harl::debug(void)
+{
+	std::cout<< "DEBUG" << std::endl;
+}
+
+
+void	Harl::info(void)
+{
+	std::cout<< "INFO" << std::endl;
+
+}
+
+
+void	Harl::warning(void)
+{
+	std::cout<< "WARNING" << std::endl;
+
+}
+
+
+void	Harl::error(void)
+{
+	std::cout<<  "ERROR" << std::endl;
+}
---
+++ c01/ex05/src/main.cpp
@@ -0,0 +1,14 @@
+
+#include"Harl.cpp"
+
+
+int	main(int argc, char **argv)
+{
+		Harl	test;
+
+		test.
+
+
+
+		return (0);
+}
