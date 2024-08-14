7adf0fe4c266f90e79e0b2865a3e9c1d18708879
Andreas Jehle <ajehle@2-D-4.42heilbronn.de>
2024-08-14 18:46:24 +0200

    working ex04

--- c01/ex04/src/main.cpp
+++ c01/ex04/src/main.cpp
@@ -1,41 +1,48 @@
 
-#include<iostream>
-#include<fstream>
-#include<iostream>
+	#include<iostream>
+	#include<fstream>
+	#include<string>
 
-int ft_replace(std::string filename, std::string str1, std::string str2)
-{
-	std::ifstream input(filename);
-	if(!input)
-		return 1;
-	std::string line;
-	while(std::getline(input, line))
+
+	int ft_replace(std::string filename, std::string str1, std::string str2)
 	{
-		line.swap(str1);
-		std::cout << line << std::endl;
-	}
-
-
-	return 0;
-}
-
-int main(int argc, char** argv)
-{
-	std::string filename;
-	std::string str1;
-	std::string str2;
-
-	if(argc == 4)
-	{
-		filename = std::string(argv[1]);
-		str1 = std::string(argv[2]);
-		str2 = std::string(argv[3]);
-		if(ft_replace(filename, str1, str2))
+		size_t pos = 0;
+		std::string line;
+		std::ifstream input(filename);
+		if(!input)
 			return 1;
-
+		while(std::getline(input, line))
+		{
+			pos = line.find(str1, pos);
+			while(pos != std::string::npos)
+			{
+				line.erase(pos, str1.length());
+				line.insert(pos, str2);
+				pos = line.find(str1, pos + str2.length());
+			}
+			std::cout<<line<<std::endl;
+		}
 		return 0;
 	}
-	else
-		return 1;
-}
+
+
+	int main(int argc, char** argv)
+	{
+		std::string filename;
+		std::string str1;
+		std::string str2;
+
+		if(argc == 4)
+		{
+			filename = std::string(argv[1]);
+			str1 = std::string(argv[2]);
+			str2 = std::string(argv[3]);
+			if(ft_replace(filename, str1, str2))
+				return 1;
+
+			return 0;
+		}
+		else
+			return 1;
+	}
 
