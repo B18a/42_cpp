44ded21fcd27cc4d5fc0b137a9a8140b46f05ee0
Andreas Jehle <ajehle@2-D-4.42heilbronn.de>
2024-08-16 19:47:15 +0200

    c03 ex00

---
+++ c03/ex00/src/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 19:22:13 by ajehle            #+#    #+#             */
+/*   Updated: 2024/08/16 19:37:27 by ajehle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+  public:
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &cpy);            // copy constuctor
+	ClapTrap &operator=(const ClapTrap &cpy); // copy assignment operator
+	~ClapTrap(void);
+
+	// member functions
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+  private:
+	std::string _name;
+	unsigned int _health;
+	unsigned int _energy;
+	unsigned int _damage;
+};
+
+#endif
---
+++ c03/ex00/src/main.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+
+#include"ClapTrap.hpp"
+
+#include <iostream>
+
+int main( void )
+{
+	ClapTrap batman("Batman");
+
+	batman.attack("Superman");
+
+	return 0;
+}
